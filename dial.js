71a2e4b366d554afa14246435691d1d9c945e100
Gcompany <115363651+JinWGcompany@users.noreply.github.com>
2023-02-11 16:24:22 +0900

    Add files via upload

---
+++ dial.js
@@ -0,0 +1,26 @@
+function setBack() {
+    var value = document.forms.output.value;
+    document.forms.output.value = value.substr(0, value.length - 1);
+}
+
+function goSite() {
+    var page = document.forms.output.value;
+    var G = 'https://jinwgcompany.github.io/Gcompany/'
+    if(page=='111'){
+        //console.log(111)
+        G = G + page
+        window.location.href="G";
+        document.forms.output.value =''
+    }
+    if(page=='5'){
+        //console.log(5)
+        G = G + 'Glive'
+        window.location.href=G;
+        document.forms.output.value =''
+    }
+    else {
+        //console.log('anot')
+        document.forms.output.value =''
+        window.location.href="https://jinwgcompany.github.io/Gcompany/";
+    }
+}
